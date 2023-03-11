c92534caae1a0f8c53acc08a5fc2e84a2d6a938a
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-03-12 01:44:30 +0300

    add distances to Order and Restaurant

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -119,8 +119,9 @@ INTERNAL_IPS = [
     '127.0.0.1'
 ]
 
-
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
     os.path.join(BASE_DIR, "bundles"),
 ]
+
+YANDEX_GEO_APIKEY = env('YANDEX_GEO_APIKEY')
