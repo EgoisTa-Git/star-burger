01faeaf243268848b476fac3604e5f154722a00d
EgoisTa <97385729+EgoisTa-Git@users.noreply.github.com>
2023-04-25 22:22:59 +0300

    Update deploy_starburger.sh

--- deploy_starburger.sh
+++ deploy_starburger.sh
@@ -3,13 +3,6 @@ set -Eeuo pipefail
 source ./venv/bin/activate
 git pull
 
-#If server is out of memory try this:
-
-#/sbin/swapoff /var/swap.1      # Uncomment for re-use without rebooting server
-#/bin/dd if=/dev/zero of=/var/swap.1 bs=1M count=1024
-#/sbin/mkswap /var/swap.1
-#/sbin/swapon /var/swap.1
-
 pip install -r requirements.txt
 npm ci --dev
 ./node_modules/.bin/parcel build bundles-src/index.js --dist-dir bundles --public-url="./"
@@ -20,7 +13,6 @@ systemctl reload starburger.service
 systemctl reload nginx.service
 
 if [ -f .env ]; then
-  # shellcheck disable=SC2046
   export $(echo $(cat .env | sed 's/#.*//g'| xargs -0) | envsubst)
 fi
 
